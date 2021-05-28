472fdf1a6fbc59eee4950fee55b95afa337b0c56
Gino-Elvis <ginoyujra38@gmail.com>
2021-05-27 21:39:13 -0500

    tarea

---
+++ proyectoGY/src/App.java
@@ -0,0 +1,10 @@
+import pe.edu.upeu.subprogramasGY;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("eres arte bb no lo olvides nunca :v");
+        subprogramasGY profeelcrak=new subprogramasGY ();
+        profeelcrak.calcularFuncionExponecial();
+       
+    }
+}
